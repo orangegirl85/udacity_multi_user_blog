93f6cbe3c518c86a9de174081e12f3859e54e32e
Nicoleta Ianosi <nicoisonai@yahoo.com>
2016-10-08 16:23:39 +0300

    Put Post class in models.py, add Helper with BlogHelper class and general functions

--- blog.py
+++ blog.py
@@ -1,59 +1,28 @@
-import os
 import re
-from string import letters
 
 import webapp2
-import jinja2
 
 from google.appengine.ext import db
+from models import Post
+import helper
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
 
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
-
-class BlogHandler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
-
-    def render_str(self, template, **params):
-        return render_str(template, **params)
-
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
-
-def render_post(response, post):
-    response.out.write('<b>' + post.subject + '</b><br>')
-    response.out.write(post.content)
-
-class MainPage(BlogHandler):
-  def get(self):
-      self.write('Hello, Udacity!')
-
-##### blog stuff
-
-def blog_key(name = 'default'):
+def blog_key(name='default'):
     return db.Key.from_path('blogs', name)
 
-class Post(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
 
-    def render(self):
-        self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", p = self)
+class MainPage(helper.BlogHandler):
+    def get(self):
+        self.write('Hello, Udacity!')
 
-class BlogFront(BlogHandler):
+
+class BlogFront(helper.BlogHandler):
     def get(self):
         posts = db.GqlQuery("select * from Post order by created desc limit 10")
-        self.render('front.html', posts = posts)
+        self.render('front.html', posts=posts)
 
-class PostPage(BlogHandler):
+
+class PostPage(helper.BlogHandler):
     def get(self, post_id):
         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
         post = db.get(key)
@@ -62,9 +31,10 @@ class PostPage(BlogHandler):
             self.error(404)
             return
 
-        self.render("permalink.html", post = post)
+        self.render("permalink.html", post=post)
 
-class NewPost(BlogHandler):
+
+class NewPost(helper.BlogHandler):
     def get(self):
         self.render("newpost.html")
 
@@ -73,7 +43,7 @@ class NewPost(BlogHandler):
         content = self.request.get('content')
 
         if subject and content:
-            p = Post(parent = blog_key(), subject = subject, content = content)
+            p = Post(parent=blog_key(), subject=subject, content=content)
             p.put()
             self.redirect('/blog/%s' % str(p.key().id()))
         else:
@@ -81,9 +51,8 @@ class NewPost(BlogHandler):
             self.render("newpost.html", subject=subject, content=content, error=error)
 
 
-
 ###### Unit 2 HW's
-class Rot13(BlogHandler):
+class Rot13(helper.BlogHandler):
     def get(self):
         self.render('rot13-form.html')
 
@@ -93,23 +62,31 @@ class Rot13(BlogHandler):
         if text:
             rot13 = text.encode('rot13')
 
-        self.render('rot13-form.html', text = rot13)
+        self.render('rot13-form.html', text=rot13)
 
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+
+
 def valid_username(username):
     return username and USER_RE.match(username)
 
+
 PASS_RE = re.compile(r"^.{3,20}$")
+
+
 def valid_password(password):
     return password and PASS_RE.match(password)
 
-EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
-class Signup(BlogHandler):
 
+class Signup(helper.BlogHandler):
     def get(self):
         self.render("signup-form.html")
 
@@ -120,8 +97,8 @@ class Signup(BlogHandler):
         verify = self.request.get('verify')
         email = self.request.get('email')
 
-        params = dict(username = username,
-                      email = email)
+        params = dict(username=username,
+                      email=email)
 
         if not valid_username(username):
             params['error_username'] = "That's not a valid username."
@@ -143,13 +120,15 @@ class Signup(BlogHandler):
         else:
             self.redirect('/unit2/welcome?username=' + username)
 
-class Welcome(BlogHandler):
+
+class Welcome(helper.BlogHandler):
     def get(self):
         username = self.request.get('username')
         if valid_username(username):
-            self.render('welcome.html', username = username)
+            self.render('welcome.html', username=username)
         else:
             self.redirect('/unit2/signup')
+
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/unit2/rot13', Rot13),
