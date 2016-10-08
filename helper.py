e8a89f9c4cde826fbb77541227f7deefc07b515c
Nicoleta Ianosi <nicoisonai@yahoo.com>
2016-10-08 17:22:45 +0300

    Add register, login, logout functionality; move BlogHandler to blog.py

--- helper.py
+++ helper.py
@@ -2,26 +2,33 @@ import os
 
 import webapp2
 import jinja2
+import hmac
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+secret = 'fart'
+
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
+
 
 def render_post(response, post):
     response.out.write('<b>' + post.subject + '</b><br>')
     response.out.write(post.content)
 
 
-class BlogHandler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
 
-    def render_str(self, template, **params):
-        return render_str(template, **params)
 
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
+
+
--- models.py
+++ models.py
@@ -3,8 +3,10 @@ entities used by the Game. Because these classes are also regular Python
 classes they can include methods (such as 'to_form' and 'new_game')."""
 
 from google.appengine.ext import db
-#from protorpc import messages
 import helper
+import random
+import hashlib
+from string import letters
 
 
 class Post(db.Model):
@@ -18,134 +20,46 @@ class Post(db.Model):
         self._render_text = self.content.replace('\n', '<br>')
         return helper.render_str("post.html", p=self)
 
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
 
-# class User(ndb.Model):
-#     """User profile"""
-#     name = ndb.StringProperty(required=True)
-#     email = ndb.StringProperty()
-#     score = ndb.IntegerProperty(default=0)
-#
-#     def to_form(self):
-#         return UserRankingForm(user_name=self.name, score=self.score)
-#
-#
-# class GameHistory(ndb.Model):
-#     """GameHistory object"""
-#     username = ndb.StringProperty(required=True)
-#     position = ndb.IntegerProperty(required=True)
-#     message = ndb.StringProperty(required=True)
-#
-#     def to_form(self):
-#         return GameHistoryForm(username=self.username, position=self.position, message=self.message)
-#
-#
-# class Game(ndb.Model):
-#     """Game object"""
-#     user1 = ndb.KeyProperty(required=True, kind='User')
-#     user2 = ndb.KeyProperty(required=True, kind='User')
-#     game_over = ndb.BooleanProperty(required=True, default=False)
-#     current_player = ndb.StringProperty(default='PLAYER_X')
-#     board = ndb.StringProperty(repeated=True)
-#     cancelled = ndb.BooleanProperty(required=True, default=False)
-#     email_reminder_sent = ndb.BooleanProperty(default=False)
-#
-#     @classmethod
-#     def new_game(cls, user1, user2):
-#         """Creates and returns a new game"""
-#         game = Game(user1=user1,
-#                     user2=user2,
-#                     board=['', '', '', '', '', '', '', '', ''])
-#         game.put()
-#         return game
-#
-#     def to_form(self, message):
-#         """Returns a GameForm representation of the Game"""
-#         form = GameForm()
-#         form.urlsafe_key = self.key.urlsafe()
-#         form.user_name1 = self.user1.get().name
-#         form.user_name2 = self.user2.get().name
-#         form.current_player = self.current_player
-#         form.board = self.board
-#         form.game_over = self.game_over
-#         form.cancelled = self.cancelled
-#         form.message = message
-#         return form
-#
-#     def end_game(self, draw=False):
-#         """Ends the game"""
-#         self.game_over = True
-#         self.put()
-#
-#         if not draw:
-#             if self.current_player == 'PLAYER_X':
-#                 self._update_user_score(self.user1, 1)
-#                 self._update_user_score(self.user2, -1)
-#             else:
-#                 self._update_user_score(self.user1, -1)
-#                 self._update_user_score(self.user2, 1)
-#
-#     def _update_user_score(self, user, score):
-#         user11 = User.query(User.name == user.get().name).get()
-#         user11.score += score
-#         user11.put()
-#
-#     def cancel_game(self):
-#         """Cancels the game"""
-#         self.cancelled = True
-#         self.put()
-#
-#
-# class GameForm(messages.Message):
-#     """GameForm for outbound game state information"""
-#     urlsafe_key = messages.StringField(1, required=True)
-#     game_over = messages.BooleanField(2, required=True)
-#     message = messages.StringField(3, required=True)
-#     user_name1 = messages.StringField(4, required=True)
-#     user_name2 = messages.StringField(5, required=True)
-#     current_player = messages.StringField(6, required=True)
-#     board = messages.StringField(7, repeated=True)
-#     cancelled = messages.BooleanField(8, required=True)
-#
-#
-# class GameForms(messages.Message):
-#     """Return multiple GameForm"""
-#     items = messages.MessageField(GameForm, 1, repeated=True)
-#
-#
-# class NewGameForm(messages.Message):
-#     """Used to create a new game"""
-#     user_name1 = messages.StringField(1, required=True)
-#     user_name2 = messages.StringField(2, required=True)
-#
-#
-# class MakeMoveForm(messages.Message):
-#     """Used to make a move in an existing game"""
-#     position = messages.IntegerField(1, required=True)
-#
-#
-# class UserRankingForm(messages.Message):
-#     """UserRankingForm for outbound User information"""
-#     user_name = messages.StringField(1, required=True)
-#     score = messages.IntegerField(2, required=True)
-#
-#
-# class UserRankingsForm(messages.Message):
-#     """Return multiple UserRankingsForms"""
-#     items = messages.MessageField(UserRankingForm, 1, repeated=True)
-#
-#
-# class GameHistoryForm(messages.Message):
-#     """GameHistoryForm for outbound GameHistory information"""
-#     username = messages.StringField(1, required=True)
-#     position = messages.IntegerField(2, required=True)
-#     message = messages.StringField(3, required=True)
-#
-#
-# class GameHistoryForms(messages.Message):
-#     """Return multiple GameHistoryForm"""
-#     items = messages.MessageField(GameHistoryForm, 1, repeated=True)
-#
-#
-# class StringMessage(messages.Message):
-#     """StringMessage-- outbound (single) string message"""
-#     message = messages.StringField(1, required=True)
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+
+def users_key(group = 'default'):
+    return db.Key.from_path('users', group)
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
